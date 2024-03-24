8c2404561b181e396a5865d33e480a0851eded23
Javier Suárez <ljsuarez7@gmail.com>
2024-03-24 09:08:36 +0100

    caso de ortografia

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
+import { GptModule } from './gpt/gpt.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    GptModule
+  ],
   controllers: [],
   providers: [],
 })
---
+++ src/gpt/use-cases/orthography.use-case.ts
@@ -0,0 +1,47 @@
+import OpenAI from "openai";
+
+interface Options {
+    prompt: string;
+}
+
+export const orthographyCheckUseCase = async(openai: OpenAI, options: Options) => {
+
+    const {prompt} = options;
+
+    const completion = await openai.chat.completions.create({
+        messages: [
+            { 
+                role: "system", 
+                content: `
+                    Te serán proveídos textos en español con posibles errores ortográficos y gramaticales. 
+                    Las palabras usadas deben existir en el diccionario de la Real Academia Española.
+                    Debes responder en formato JSON,
+                    tu tarea es corregirlos y devolver información de las solucitudes,
+                    también debes dar un porcentaje de acierto al usuario,
+                    Si no hay errores, debes devolver un mensaje de felicitación.
+                    Ejemplo de salida:
+                    {
+                        userScore: number,
+                        errors: string[], // ['error -> solución']
+                        message: string, //Usa emojis y texto para felicitar al usuario
+                    }
+                ` 
+            },
+            {
+                role: "user",
+                content: prompt
+            }
+        ],
+        model: "gpt-3.5-turbo",
+        temperature: 0.3,
+        max_tokens: 150,
+        response_format: {
+            type: 'json_object'
+        }
+    });
+
+    const jsonResp = JSON.parse(completion.choices[0].message.content);
+
+    return jsonResp;
+
+}
